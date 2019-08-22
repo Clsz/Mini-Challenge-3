dec530140586d93b754e833da8056acc3c4aafb6
Clsz <muhammad.reynaldi001@binus.ac.id>
2019-08-22 14:57:41 +0700

    Add Activities

--- Mini Challenge 3/Dashboard/Controller/DashboardViewController.swift
+++ Mini Challenge 3/Dashboard/Controller/DashboardViewController.swift
@@ -9,22 +9,43 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
-
+    // Variables
+    let sortId = "sortID"
+    var sortImages = [UIImage(named: "plastik"), UIImage(named: "metal"), UIImage(named: "kayu"), UIImage(named: "kaca"), UIImage(named: "kertas")]
+    
+    //Outlet
+    @IBOutlet weak var sortCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        cellDelegate()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func notifTapped(_ sender: UIButton) {
     }
-    */
+    
+}
 
+extension DashboardViewController:UICollectionViewDataSource, UICollectionViewDelegate{
+    
+    func cellDelegate(){
+        sortCollectionView.dataSource = self
+        sortCollectionView.delegate = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sortImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: sortId, for: indexPath) as! SortInfoCollectionViewCell
+        cell.sortImage.image = sortImages[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //performSegue(withIdentifier: <#T##String#>, sender: <#T##Any?#>)
+    }
+    
 }
---
+++ Mini Challenge 3/Dashboard/Controller/SortInfoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SortInfoCollectionViewCell.swift
+//  Mini Challenge 3
+//
+//  Created by Muhammad Reynaldi on 22/08/19.
+//  Copyright © 2019 11. All rights reserved.
+//
+
+import UIKit
+
+class SortInfoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sortImage: UIImageView!
+}
