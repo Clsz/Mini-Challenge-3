440d98c78717b7781b6a1e3e12a02442bf054cac
Steven Gunawan <stevengunawan1512@gmail.com>
2019-08-30 19:20:12 +0700

    30 Aug 19:20

--- Mini Challenge 3/List Recyclable/Controller/ListRecyclableTableViewCell.swift
+++ Mini Challenge 3/List Recyclable/Controller/ListRecyclableTableViewCell.swift
@@ -12,55 +12,54 @@ import CloudKit
 class ListRecyclableTableViewCell: UITableViewCell {
     
     var listSampah = [CKRecord]()
+    var imageSampah: UIImage?
+    
     @IBOutlet weak var listSampahCV: UICollectionView!
     @IBOutlet weak var jenisSampah: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-//        cellDelegate2()
+        listSampahCV.delegate = self
+        listSampahCV.dataSource = self
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
     
 }
 
+extension ListRecyclableTableViewCell:UICollectionViewDataSource,UICollectionViewDelegate{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return listSampah.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LRecyclableCVCell", for: indexPath) as! ListRecyclableCollectionViewCell
+        
+        let harga = listSampah[indexPath.row].value(forKey: "wastePrice") as? Int
+        cell.hargaSampah.text = "Rp.\(harga ?? 0),-/kg"
+        cell.namaSampah.text = listSampah[indexPath.row].value(forKey: "wasteName") as? String
+        cell.gambarSampah.image = imageSampah
+        
+        return cell
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let cellsAcross: CGFloat = 3
+        let spaceBetweenCells: CGFloat = 5
+        let dim = (collectionView.bounds.width - (cellsAcross - 1) * spaceBetweenCells) / cellsAcross
+        return CGSize(width: dim, height: dim)
+        
+    }
+    
+}
 
 
-    
-    
-    //    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    //
-    //    }
-    
-//    func cellDelegate2(){
-//        listSampahCV.dataSource =  self
-//        listSampahCV.delegate = self
-//    }
-
-//func queryDatabase() {
-//    let query = CKQuery(recordType: "Waste", predicate: NSPredicate(value: true))
-//    CKContainer.init(identifier: "iCloud.Cls.MC3").publicCloudDatabase.perform(query, inZoneWith: nil) { (records, _) in
-//        guard let records = records else { return }
-//        //            print(records)
-//        let sortedRecords = records.sorted(by: { $1.modificationDate! > $0.modificationDate! })
-//        self.listSampah = sortedRecords
-//        DispatchQueue.main.async {
-//            self.listSampahCV.reloadData()
-//        }
-//    }
-//}
-//
-//extension ListRecyclableTableViewCell
-//{
-//    func setCollectionViewDataSourceDelegate
-//        <D: UICollectionViewDelegate & UICollectionViewDataSource>
-//        (_  dataSourceDelegate: D, forRow row: Int)
-//    {
-//        listSampahCV.delegate = dataSourceDelegate
-//        listSampahCV.dataSource = dataSourceDelegate
-//        
-//        listSampahCV.reloadData()
-//    }
-//}
--- Mini Challenge 3/List Recyclable/Controller/ListRecyclableViewController.swift
+++ Mini Challenge 3/List Recyclable/Controller/ListRecyclableViewController.swift
@@ -9,57 +9,31 @@
 import UIKit
 import CloudKit
 
-protocol ReusableViewEnum {}
-class ListRecyclableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+class ListRecyclableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var plasticImageArray = [String]()
-    var metalImageArray = [String]()
-    var paperImageArray = [String]()
-    var glassImageArray = [String]()
-    var othersImageArray = [String]()
     
-    let cloudDatabase = CKContainer.default().publicCloudDatabase
+    let cloudDatabase = CKContainer(identifier: "iCloud.Cls.MC3").publicCloudDatabase
     var listNamaSampah = [CKRecord]()
-//    var  listJenisSampah = [CKRecord]()
+    var sampahGambar = [UIImage]()
     
     let  cellList = "listSampah"
-    var name = [String]()
-    var price = [String]()
-    var type = [String]()
     
-    var  category = [String]()
-    var categoryPlastik = [String]()
-    var categoryMetal = [String]()
-    var categoryPaper = [String]()
-    var categoryGlass = [String]()
-    var categoryOthers = [String]()
-    
+    var types = ["Plastic", "Paper", "Metal", "Glass", "Others"]
+    var contents : [[CKRecord]] = [[], [], [], [], []]
+    var i = 0
     
     @IBOutlet weak var listSampahTV: UITableView!
     
     override func viewDidLoad() {
-                queryDatabase()
+        //                queryDatabase()
         self.navigationItem.title = "Recyclable List"
         super.viewDidLoad()
-        //       cellDelegate()
         
-        category =  ["plastik","metal","paper","glass","others"]
-        categoryPlastik = [""]
-        categoryMetal = [""]
-        categoryPaper = [""]
-        categoryGlass = [""]
-        categoryOthers = [""]
+        setImage()
         
-        plasticImageArray = ["p1","p2","p3","p4"]
-        metalImageArray = ["m1","m2","m3","m4"]
-        paperImageArray = ["pa1","pa2","pa3"]
-        glassImageArray = ["g1"]
-        othersImageArray = ["o1","o2","o3"]
-        
-        
-        type = ["Plastik","Metal","Fire","Water","Paper"]
-        name = ["plastik","metal","fire","water","paper"]
-        price = ["plastik","metal","fire","water","paper"]
+        for i in  0..<types.count{
+            getOne(type: types[i])
+        }
         
     }
     
@@ -67,110 +41,78 @@ class ListRecyclableViewController: UIViewController, UITableViewDataSource, UIT
         navigationController?.navigationBar.isHidden = false
     }
     
-
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return plasticImageArray.count
-        
+        return contents.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell =  tableView.dequeueReusableCell(withIdentifier: "LRecyclableTVCell", for: indexPath) as! ListRecyclableTableViewCell
+ 
+        cell.jenisSampah.text = types[indexPath.row]
+        cell.listSampah = contents[indexPath.row]
+        cell.imageSampah = sampahGambar[indexPath.row]
         
-//        cell.jenisSampah.text = type[indexPath.row]
-        cell.jenisSampah.text = listNamaSampah[indexPath.row].value(forKey: "wasteCategory") as? String
-        cell.listSampahCV.reloadData()
+        DispatchQueue.main.async {
+            
+            cell.listSampahCV.reloadData()
+        }
         
         
         return cell
         
     }
     
-        func queryDatabase() {
-            let query = CKQuery(recordType: "Waste", predicate: NSPredicate(value: true))
-            CKContainer.init(identifier: "iCloud.Cls.MC3").publicCloudDatabase.perform(query, inZoneWith: nil) { (records, _) in
-                guard let records = records else { return }
-                let sortedRecords = records.sorted(by: { $1.modificationDate! > $0.modificationDate! })
-                self.listNamaSampah = sortedRecords
-                DispatchQueue.main.async {
-                    self.listSampahTV.reloadData()
+    //        func queryDatabase() {
+    //            let query = CKQuery(recordType: "Waste", predicate: NSPredicate(value: true))
+    //            CKContainer.init(identifier: "iCloud.Cls.MC3").publicCloudDatabase.perform(query, inZoneWith: nil) { (records, _) in
+    //                guard let records = records else { return }
+    //                let sortedRecords = records.sorted(by: { $1.modificationDate! > $0.modificationDate! })
+    //                self.listNamaSampah = sortedRecords
+    //                DispatchQueue.main.async {
+    //                    self.listSampahTV.reloadData()
+    //                }
+    //            }
+    //        }
+    
+    func getOne(type: String){
+        let pred = NSPredicate(format: "wasteCategory IN %@", [type])
+        let query = CKQuery(recordType: "Waste", predicate: pred)
+        //        let sort = NSSortDescriptor(key: "lastModifiedUserRecordID", ascending: false)
+        //        query.sortDescriptors = [sort]
+        let queryOperation = CKQueryOperation (query: query)
+        queryOperation.recordFetchedBlock = {
+            record in
+            print("\(record)")
+            
+            if let i = self.types.firstIndex(of: type){
+                self.contents[i].append(record)
+            }
+        }
+        queryOperation.queryCompletionBlock = {
+            queryCursor, error in
+            if error != nil {
+                print("\(String(describing: error))")
+            }
+            DispatchQueue.main.async {
+                self.listSampahTV.reloadData()
+            }
+        }
+        cloudDatabase.add(queryOperation)
+    }
+    
+    func setImage(){
+        for i in listNamaSampah {
+            if let data = i["wastePictures"] as? [CKAsset] {
+                for i in data{
+                    if let img = i.toUIImage(){
+                        sampahGambar.append(img)
+                    }
                 }
             }
         }
-    
-    
+    }
 }
 
-extension ListRecyclableViewController: UICollectionViewDataSource, UICollectionViewDelegate{
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return plasticImageArray.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LRecyclableCVCell", for: indexPath) as! ListRecyclableCollectionViewCell
-        //
-        //        let harga = listNamaSampah[indexPath.row].value(forKey: "wastePrice") as? Int
-        //        cell.hargaSampah.text = "Rp.\(harga ?? 0),-/kg"
-        //        cell.namaSampah.text = listNamaSampah[indexPath.row].value(forKey: "wasteName") as? String
-        //
-        //
-        cell.gambarSampah.image = UIImage(named: plasticImageArray[indexPath.row])
-        cell.namaSampah.text = name[indexPath.row]
-        cell.hargaSampah.text = price[indexPath.row]
-        return cell
-        
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let cellsAcross: CGFloat = 3
-        let spaceBetweenCells: CGFloat = 5
-        let dim = (collectionView.bounds.width - (cellsAcross - 1) * spaceBetweenCells) / cellsAcross
-        return CGSize(width: dim, height: dim)
-        
-    }
-    
-    
-    
-}
-
-
-//extension ListRecyclableViewController: UICollectionViewDataSource,UICollectionViewDelegate{
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return listSampah.count
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "listJenisSampah", for: indexPath) as! ListRecyclableCollectionViewCell
-//
-//        cell.jenisSampah.text = listSampah[indexPath.row].value(forKey: "wasteCategory") as? String
-//
-//        //        cell.gambarSampah.image = listSampah[indexPath.row].value(forKey: "wastePicture") as? UIImage
-//        return cell
-//    }
-//}
-//
-//extension ListRecyclableViewController:UITableViewDataSource, UITableViewDelegate{
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: cellList, for: indexPath).view
-//
-//        return cell
-//    }
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 5
-//    }
-//
-//    func cellDelegate(){
-//        listSampahTV.dataSource = self
-//        listSampahTV.delegate = self
-//    }
-//
-//}
 
